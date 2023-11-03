7d5adbf46751a6d9ad5a0574e11f504a7d0bdc66
S M A Tousi <mohamadali.tousi@gmail.com>
2023-11-03 15:39:39 -0500

    sadf

--- run_test.py
+++ run_test.py
@@ -17,3 +17,46 @@ import numpy as np
 torch.manual_seed(1234)
 random.seed(1234)
 np.random.seed(1234)
+
+parser = argparse.ArgumentParser(description="A script with argparse options")
+
+# Add an argument for an integer option
+parser.add_argument("--runname", type=str, required=True)
+parser.add_argument("--projectname", type=str, required=True)
+parser.add_argument("--modelname", type=str, required=True)
+parser.add_argument("--batchsize", type=int, default=4)
+parser.add_argument("--savingstep", type=int, default=10)
+parser.add_argument("--epochs", type=int, default=100)
+parser.add_argument("--nottest", help="Enable verbose mode", action="store_true")
+
+args = parser.parse_args()
+
+arg_batch_size = args.batchsize
+arg_epochs = args.epochs
+arg_runname = args.runname
+arg_projectname = args.projectname
+arg_modelname = args.modelname
+arg_savingstep = args.savingstep
+
+if args.nottest:
+    arg_nottest = True 
+else:
+    arg_nottest = False
+
+run = wandb.init()
+
+artifact = run.use_artifact('tousi-team/test1/model_epoch_1:v0', type='model')
+artifact_dir = artifact.download()
+
+if arg_modelname == 'Unet':
+        model = UNet(n_channels=18, n_classes=1).to(device)  # Change n_classes based on your output
+    
+
+
+model.load_state_dict(artifact_dir)
+
+
+print("S!")
+
+
+
