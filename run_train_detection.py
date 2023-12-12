98ecaced6c9c4b5bbd1d7354438309c4a640d100
Seyed Mohamad Ali Tousi <77631699+SMATousi@users.noreply.github.com>
2023-12-12 12:42:07 -0600

    Update run_train_detection.py

--- run_train_detection.py
+++ run_train_detection.py
@@ -1,5 +1,29 @@
 from model import *
 
+in_dir = '/root/home/128x128_raw/rgb_images/'
+tar_dir = '/root/home/128x128_raw/ground_truth/'
+
+dataset = RGBGroundTruthDataset(rgb_dir=in_dir, gt_dir=tar_dir, years=['0','1','2','3','4','5'])
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=2,num_workers=8, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=2,num_workers=8, shuffle=True)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = SingleResNetLSTMUNet(num_classes=1, hidden_dim=6, lstm_layers=10)
+model = model.to(device)  # Assuming you have a device (like 'cuda' or 'cpu')
+
+# Loss Function
+criterion = nn.BCEWithLogitsLoss()
+
+# Optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
 num_epochs = 1
 
 for epoch in range(num_epochs):
